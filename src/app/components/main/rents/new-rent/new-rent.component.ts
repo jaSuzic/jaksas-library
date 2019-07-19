3421edad89246aaf8b66c0018634a745cb5d9b7b
Jaksa Suzic <jaksa.suzic@eng.it>
2019-07-19 16:50:18 +0200

    123

--- src/app/components/main/rents/new-rent/new-rent.component.ts
+++ src/app/components/main/rents/new-rent/new-rent.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material';
 import { Book } from 'src/app/models/book.model';
 import { Member } from 'src/app/models/member.model';
+
+import { APP_DATE_FORMATS, AppDateAdapter } from './../../../../helpers/format-datepicker';
 
 @Component({
   selector: "app-new-rent",
   templateUrl: "./new-rent.component.html",
-  styleUrls: ["./new-rent.component.css"]
+  styleUrls: ["./new-rent.component.css"],
+  providers: [
+    { provide: DateAdapter, useClass: AppDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS }
+  ]
 })
 export class NewRentComponent implements OnInit {
   chosenMember: Member;
   chosenBook: Book;
+  chosenDate: Date = new Date();
+  memberForm: FormGroup;
+  bookForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.memberForm = new FormGroup({
+      memberId: new FormControl("", Validators.required)
+    });
+    this.bookForm = new FormGroup({
+      bookId: new FormControl("", Validators.required)
+    });
+  }
 
   choseMember(row) {
     this.chosenMember = {
@@ -22,8 +40,10 @@ export class NewRentComponent implements OnInit {
       birthDate: row.birthDate,
       _id: row._id
     };
+    this.memberForm.get("memberId").setValue(row._id);
   }
   choseBook(book) {
     this.chosenBook = book;
+    this.bookForm.get("bookId").setValue(book._id);
   }
 }
